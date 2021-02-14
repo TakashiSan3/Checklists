af93bcecbf4b2d8d099d212bce37a2d49e79d34e
TakashiSan <takashi_fujita_web@icloud.com>
2021-02-14 18:06:08 +0900

    Add some files

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,12 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
